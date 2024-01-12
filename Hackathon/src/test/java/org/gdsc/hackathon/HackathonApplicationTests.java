cc255dce5ffffce8cc3298be5490f71bf4ef3e47
Kim, Seokwan <sukwan1106@gmail.com>
2024-01-12 16:26:42 +0900

    first

---
+++ Hackathon/src/test/java/org/gdsc/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdsc.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
